f87757b171dccc59d36ba5adf012dbb4b1a30ee3
git-clover <minor_0129@naver.com>
2026-08-01 03:55:40 +0900

    Kicked out Orca Turquoise and brought in IDOLABS Royal Blue: Season VII

--- src/slic3r/GUI/Notebook.cpp
+++ src/slic3r/GUI/Notebook.cpp
@@ -139,7 +139,7 @@ void ButtonsListCtrl::SetSelection(int sel)
 
     StateColor bg_color = StateColor(
         std::pair{wxColour(0, 55, 133), (int) StateColor::Hovered},
-        std::pair{wxColour(0,150, 136), (int) StateColor::Normal});
+        std::pair{wxColour(0, 56, 135), (int) StateColor::Normal});
     m_pageButtons[m_selection]->SetBackgroundColor(bg_color);
 
     StateColor text_color = StateColor(
